cdc09434ea78a1af55fece48a0b3210ab20566ce
Sammy Lee <sammylee@Sammys-MacBook-Air.local>
2019-02-14 16:59:13 -0800

    updated

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="lambdata-captmoonshot",
-	version="0.0.14",
+	version="0.0.15",
 	author="captmoonshot",
 	description="A collection of Data Science helper functions",
 	long_description=LONG_DESCRIPTION,
